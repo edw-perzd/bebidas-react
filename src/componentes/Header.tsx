457a09fafa63f79bd24fbfb213123aaf613e78b6
Eduardo <legna246.dav@gmail.com>
2025-02-06 11:41:45 -0600

    02 Muestra formulario y background en Home

---
+++ src/componentes/Header.tsx
@@ -0,0 +1,73 @@
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+export default function Header() {
+  const { pathname } = useLocation()
+  const isHome = useMemo(() => pathname === '/', [pathname])
+  console.log(isHome)
+
+  return (
+    <>
+    <header className={isHome? 'bg-header bg-cover bg-center': 'bg-slate-800'}>
+        <div className="mx-auto container px-5 py-16">
+            <div className="flex justify-between items-center">
+                <div>
+                    <img className="w-32" src="/logo.svg" alt="logotipo" />
+                </div>
+
+                <nav>
+                  <NavLink 
+                    className={({isActive}) => isActive ? 
+                    "text-orange-400 uppercase":
+                    "text-white uppercase"
+                    }
+                    to='/'>Home</NavLink>
+                  <NavLink 
+                    className={({isActive}) => isActive ? 
+                    "text-orange-400 uppercase":
+                    "text-white uppercase"
+                    }
+                    to='/favorites'>Favoritos</NavLink>
+                </nav>
+            </div>
+            {isHome && (
+              <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+              <div className="space-y-4">
+                <label 
+                  htmlFor="ingredient"
+                  className="block text-white uppercase font-extrabold text-lg">
+                    Nombre o Ingredientes
+                  </label>
+                  <input 
+                    id='ingredient'
+                    type="text" 
+                    name="ingredient"
+                    className="p-3 w-full rounded-lg focus:outline-none"
+                    placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila Café"
+                    />
+              </div>
+              <div className="space-y-4">
+                <label 
+                  htmlFor="category"
+                  className="block text-white uppercase font-extrabold text-lg">
+                    Categoría
+                  </label>
+                  <select 
+                    id='category'
+                    name="category"
+                    className="p-3 w-full rounded-lg focus:outline-none"
+                    >
+                      <option value="">-- Seleccione --</option>
+                    </select>
+              </div>
+              <input 
+                type="submit"
+                className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" 
+                value="Buscar Recetas" />
+            </form>
+            )}
+        </div>
+    </header>
+    </>
+  )
+}
