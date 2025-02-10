57742e5faa0fb9f904bd7bd6b4bc920e1217abe6
Eduardo <legna246.dav@gmail.com>
2025-02-10 13:57:29 -0600

    06 Show Recipies in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,28 @@
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function HomePage(){
+
+    const recipes = useAppStore(state => state.recipes)
+    const hasRecipes = useMemo(() => recipes.drinks.length > 0, [recipes])
+
     return(
         <>
-            <h1>Home Page</h1>
+            <h1>Recetas</h1>
+            {hasRecipes? (
+                <>
+                <p>Si hay recetas</p>
+                <ul>
+                    {
+                        recipes.drinks.map(drink => (
+                            <li key={drink.idDrink}>{drink.strDrink}</li>
+                        ))
+                    }
+                </ul>
+                </>
+            ): (
+                <p>No hay recetas todavia, busca con el formulario</p>
+            )}
         </>
     )
 }
