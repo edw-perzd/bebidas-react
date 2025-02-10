fe2bbc92e6439ff9a15a9b80f8ff453de14bfc13
Eduardo <legna246.dav@gmail.com>
2025-02-10 11:25:23 -0600

    05 Show categories in select

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { useAppStore } from "../stores/useAppStore";
 
 export default function Header() {
   const { pathname } = useLocation()
@@ -8,6 +9,14 @@ export default function Header() {
     ingredient: '',
     category: ''
   })
+
+  const categories = useAppStore((state) => state.categories)
+  const fetchCategories = useAppStore((state) => state.fetchCategories)
+  console.log(categories.drinks)
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
 
   function handleChange(e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>){
     setSearchFilters({
@@ -69,6 +78,11 @@ export default function Header() {
                     className="p-3 w-full rounded-lg focus:outline-none"
                     >
                       <option value="">-- Seleccione --</option>
+                      {
+                        categories.drinks.map(category => (
+                          <option key={category.strCategory} value={category.strCategory}>{category.strCategory}</option>
+                        ))
+                      }
                     </select>
               </div>
               <input 
